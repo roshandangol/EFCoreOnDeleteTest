e73e4323027a90c2693d5b47dd6cf59e6f79120b
Roshan Dangol <roshan.dangol@hotmail.com>
2020-06-13 18:24:54 +0545

    log to event veiwer log to file done

--- EFCoreOnDeleteTest/Program.cs
+++ EFCoreOnDeleteTest/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.EventLog;
+using NLog.Extensions.Logging;
 
 namespace EFCoreOnDeleteTest
 {
@@ -10,38 +9,42 @@ namespace EFCoreOnDeleteTest
     {
         public static void Main(string[] args)
         {
+            
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            
             .ConfigureLogging((hostingContext,logging) =>
             {
 
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddEventLog(new EventLogSettings()
-                {
-                    SourceName = "EFCoreTestLog",
-                    LogName = "EFCoreTestLog",
-                    Filter = (x, y) => y >= LogLevel.Information
-                });
+                //logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                //logging.AddEventLog(new EventLogSettings()
+                //{
+                //    SourceName = "EFCoreTestLog",
+                //    LogName = "EFCoreTestLog",
+                //    Filter = (x, y) => y >= LogLevel.Information
+                //});
                 // clear default logging providers
                 logging.ClearProviders();
 
                 // add built-in providers manually, as needed 
                 logging.AddConsole();
                 logging.AddDebug();
-                //logging.AddEventLog();
+                logging.AddEventLog();
                 logging.AddEventSourceLogger();
+                logging.AddNLog();
 
 
 
 
             })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                ;
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            ;
     }
 }
